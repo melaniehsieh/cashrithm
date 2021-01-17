80b086d0ab56ac3423a93dba0eecda5bcbebd84d
Melanie Hsieh <mpwh2004@gmail.com>
2021-01-17 09:24:44 +0800

    started landing page

--- cashrithm-frontend/src/components/Landing/Landing.js
+++ cashrithm-frontend/src/components/Landing/Landing.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./styles.css";
 
-const Landing = ({history}) => {
+const Landing = () => {
   return (
     <div>
-      <h4>Landing</h4>
+      <h1>
+        discover the <br /> experience
+      </h1>
+      <h4>
+        a platform dedicated to startups <br /> provide in-depth personalized
+        summary
+      </h4>
+      <button className="btn">Get Started</button>
     </div>
   );
 };
